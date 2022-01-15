6506f60b25edbd3f367296a5b7de122c7e84e1ec
romainsilvy <romain.silvy06@gmail.com>
2022-01-15 20:33:23 +0100

    change bg add btn

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,21 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.QuitBtn = new System.Windows.Forms.Button();
+            this.OptionBtn = new System.Windows.Forms.Button();
+            this.PlayBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // QuitBtn
+            // 
+            this.QuitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.QuitBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.QuitBtn.Location = new System.Drawing.Point(338, 465);
+            this.QuitBtn.Name = "QuitBtn";
+            this.QuitBtn.Size = new System.Drawing.Size(112, 38);
+            this.QuitBtn.TabIndex = 3;
+            this.QuitBtn.Text = "QUIT";
+            this.QuitBtn.UseVisualStyleBackColor = false;
+            this.QuitBtn.Click += new System.EventHandler(this.QuitBtn_Click);
+            // 
+            // OptionBtn
+            // 
+            this.OptionBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.OptionBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.OptionBtn.Location = new System.Drawing.Point(338, 384);
+            this.OptionBtn.Name = "OptionBtn";
+            this.OptionBtn.Size = new System.Drawing.Size(112, 38);
+            this.OptionBtn.TabIndex = 4;
+            this.OptionBtn.Text = "OPTION";
+            this.OptionBtn.UseVisualStyleBackColor = false;
+            this.OptionBtn.Click += new System.EventHandler(this.OptionBtn_Click_1);
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.PlayBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.PlayBtn.Location = new System.Drawing.Point(338, 298);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(112, 38);
+            this.PlayBtn.TabIndex = 5;
+            this.PlayBtn.Text = "PLAY";
+            this.PlayBtn.UseVisualStyleBackColor = false;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click_1);
+            this.PlayBtn.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PlayBtn_KeyPress);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(462, 751);
+            this.Controls.Add(this.PlayBtn);
+            this.Controls.Add(this.OptionBtn);
+            this.Controls.Add(this.QuitBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button QuitBtn;
+        private System.Windows.Forms.Button OptionBtn;
+        private System.Windows.Forms.Button PlayBtn;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,5 +21,31 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void PlayBtn_Click_1(object sender, EventArgs e)
+        {
+            Console.WriteLine("play");
+        }
+
+        private void OptionBtn_Click_1(object sender, EventArgs e)
+        {
+            //this.Hide();
+
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar);
+        }
+
+        private void PlayBtn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Console.WriteLine(e.KeyChar);
+        }
     }
 }
