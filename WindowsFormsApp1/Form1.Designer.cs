c07045386e624331f771b9aa55e0b0f7b355b349
romainsilvy <romain.silvy06@gmail.com>
2022-01-30 22:51:35 +0100

    picturebox transparent

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -76,6 +76,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Location = new System.Drawing.Point(12, 72);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(320, 667);
