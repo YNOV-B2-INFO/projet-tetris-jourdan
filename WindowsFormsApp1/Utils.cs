3e408483cb7b1a89db9920e0d10962c9dcf24e81
romainsilvy <romain.silvy06@gmail.com>
2022-02-17 10:38:32 +0100

    utils.cs done

--- WindowsFormsApp1/Utils.cs
+++ WindowsFormsApp1/Utils.cs
@@ -7,11 +7,27 @@ namespace WindowsFormsApp1
 {
     internal class Utils
     {
+        //color to fill the piece 
+        System.Drawing.SolidBrush[] allBrushes = new System.Drawing.SolidBrush[]
+            {
+                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 243, 2, 125)),
+                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 252, 139, 80)),
+                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 83, 36, 240)),
+                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 17, 181, 228)),
+                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 86, 227, 159))
+            };
+        //color to draw the border of the piece 
+        System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Black);
+
+        //return a random piece object with the pieceNumber set 
         public Pieces CreatePieceObject()
         {
+            //set the range of piece generation here 
             Pieces currentPiece = null;
             Random rand = new Random();
             int pieceNumber = rand.Next(2, 5);
+
+            //create the object here 
             switch (pieceNumber)
             {
                 case 1:
@@ -32,50 +48,24 @@ namespace WindowsFormsApp1
                 default:
                     break;
             }
+
+            //set the pieceNumber
             currentPiece.pieceNumber = pieceNumber;
             return currentPiece;
         }
 
-        public void DisplayGrid(int[,] grid)
+        //display the grid based on the numbers in the cases 
+        public void DisplayGridGraphics(int[,] grid, PictureBox pictureBox1)
         {
-            for (int i = 0; i < grid.GetLength(0); i++)
-            {
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    Console.Write(grid[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public void DisplayGridGraphics(Pieces currentPiece, int[,] grid, PictureBox pictureBox1)
-        {
-            System.Drawing.SolidBrush myBrush = currentPiece.color;
+            //bitmap and canva to draw the piece
             Bitmap draw = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             Graphics canvas = Graphics.FromImage(draw);
-            System.Drawing.SolidBrush[] allBrushes = new System.Drawing.SolidBrush[]
-            {
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 243, 2, 125)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 252, 139, 80)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 83, 36, 240)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 17, 181, 228)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 86, 227, 159))
-            };
-            System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Black);
 
+            //test all cases of the grid 
             for (int y = 0; y < grid.GetLength(0); y++)
             {
                 for (int x = 0; x < grid.GetLength(1); x++)
                 {
-                    switch (grid[y, x])
-                    {
-                        case 0:
-                            break;
-                        case 1:
-
-                        default:
-                            break;
-                    }
                     if (grid[y,x] != 0)
                     {
                         canvas.FillRectangle(allBrushes[grid[y,x] - 1], x * 32, y * 32, 32, 32);
@@ -86,73 +76,52 @@ namespace WindowsFormsApp1
             pictureBox1.Image = draw;
         }
 
+        //display the piece given in parameters in the picturebox given in parameters 
+        public void DisplayOnePiece(Pieces nextPiece, PictureBox pictureBox)
+        {
+            Bitmap draw = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics canvas = Graphics.FromImage(draw);
+
+            for (int y = 0; y < nextPiece.coordinates.GetLength(0); y++)
+            {
+                canvas.FillRectangle(allBrushes[nextPiece.pieceNumber - 1], nextPiece.coordinates[y, 0] * 25 - 80, nextPiece.coordinates[y, 1] * 25 + 60, 25, 25);
+                canvas.DrawRectangle(pen, nextPiece.coordinates[y, 0] * 25 - 80, nextPiece.coordinates[y, 1] * 25 + 60, 25, 25);
+            }
+            pictureBox.Image = draw;
+        }
+
+        //moove the piece based on the direction given parameters 
         public void Moove(string direction, Pieces currentPiece, Grid grid, PictureBox pictureBox1, Form form1)
         {
+            DisplayGridGraphics(grid.GetGrid(), pictureBox1);
+            grid.RemovePrevCoordinates(currentPiece.coordinates);
             switch (direction)
             {
                 case "left":
-                    DisplayGridGraphics(currentPiece, grid.GetGrid(), pictureBox1);
-                    grid.RemovePrevCoordinates(currentPiece.coordinates);
                     currentPiece.GoLeft(grid.GetGrid());
-                    grid.SetPieceWithCoordinates(currentPiece.coordinates, currentPiece.pieceNumber);
-                    form1.Refresh();
                     break;
                 case "right":
-                    DisplayGridGraphics(currentPiece, grid.GetGrid(), pictureBox1);
-                    grid.RemovePrevCoordinates(currentPiece.coordinates);
                     currentPiece.GoRight(grid.GetGrid());
-                    grid.SetPieceWithCoordinates(currentPiece.coordinates, currentPiece.pieceNumber);
-                    form1.Refresh();
                     break;
                 case "down":
-                    DisplayGridGraphics(currentPiece, grid.GetGrid(), pictureBox1);
-                    grid.RemovePrevCoordinates(currentPiece.coordinates);
                     currentPiece.GoDown(grid.GetGrid());
-                    grid.SetPieceWithCoordinates(currentPiece.coordinates, currentPiece.pieceNumber);
-                    form1.Refresh();
                     break;
                 case "rotate":
-                    DisplayGridGraphics(currentPiece, grid.GetGrid(), pictureBox1);
-                    grid.RemovePrevCoordinates(currentPiece.coordinates);
                     currentPiece.Rotate();
                     currentPiece.CheckBorder(grid.GetGrid());
-                    grid.SetPieceWithCoordinates(currentPiece.coordinates, currentPiece.pieceNumber);
-                    form1.Refresh();
                     break;
                 default:
                     break;
             }
+            grid.SetPieceWithCoordinates(currentPiece.coordinates, currentPiece.pieceNumber);
+            form1.Refresh();
         }
 
-        public void playSimpleSound()
+        //play the soundLoop 
+        public void PlaySimpleSound()
         {
             SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.choriste);
             simpleSound.PlayLooping();
-        }
-
-        public void PreviewPiece(Pieces nextPiece, int[,] grid, PictureBox pictureBox2)
-        {
-            System.Drawing.SolidBrush myBrush = nextPiece.color;
-            Bitmap draw = new Bitmap(pictureBox2.Width, pictureBox2.Height);
-            Graphics canvas = Graphics.FromImage(draw);
-            System.Drawing.SolidBrush[] allBrushes = new System.Drawing.SolidBrush[]
-            {
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 243, 2, 125)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 252, 139, 80)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 83, 36, 240)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 17, 181, 228)),
-                new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, 86, 227, 159))
-            };
-            System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Black);
-
-            for (int y = 0; y < nextPiece.coordinates.GetLength(0); y++)
-            {
-
-                canvas.FillRectangle(allBrushes[nextPiece.pieceNumber - 1], nextPiece.coordinates[y, 0] * 25 - 80, nextPiece.coordinates[y, 1] * 25 + 60, 25, 25);
-                canvas.DrawRectangle(pen, nextPiece.coordinates[y, 0] * 25 - 80, nextPiece.coordinates[y, 1] * 25 + 60, 25, 25);
-
-            }
-            pictureBox2.Image = draw;
         }
     }
 }
